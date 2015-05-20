eef7c636982ae259cb49a2faa8b01602410640fa
Miguel Ávila Monroy <m.avilam@alumnos.urjc.es>
2015-05-20 10:55:16 +0200

    Cambio de clave

--- index.js
+++ index.js
@@ -383,7 +383,7 @@ $(document).ready(function() {
 
 	$("#read").click(function(){
 		hello.init({
-			github : "cd7198e82aa5a52743ce"
+			github : "178bbf1a96ea6ca75380"
 	    },{
 			redirect_uri : 'redirect.html',
 			oauth_proxy : "https://auth-server.herokuapp.com/proxy",
